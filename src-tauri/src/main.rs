d3923b5c126da66d06577bdd5841001db8a566db
Andeya <andeyalee@outlook.com>
2023-12-21 05:10:30 +0800

    first
    Change-Id: I2b6ff465f6557d0a88558201fecfa9a45d01ce85

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,80 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(focused) => {
+                let item_handle = event.window().app_handle().tray_handle().get_item("hide");
+                if focused.to_owned() {
+                    item_handle.set_title("Hide").unwrap();
+                } else {
+                    item_handle.set_title("Show").unwrap();
+                }
+            }
+            _ => {}
+        })
+        .system_tray(new_system_tray())
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a left click");
+            }
+            SystemTrayEvent::RightClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a right click");
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a double click");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    println!("is_focused:{}", window.is_focused().unwrap());
+                    if window.is_focused().unwrap() {
+                        window.minimize().unwrap();
+                    } else {
+                        window.set_focus().unwrap();
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+fn new_system_tray() -> SystemTray {
+    // 这里 `"quit".to_string()` 定义菜单项 ID，第二个参数是菜单项标签。
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+    SystemTray::new().with_menu(tray_menu)
+}
